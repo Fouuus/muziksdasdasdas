011a5db502eac9e3ad374103177c238c397632cb
Fouuus <124574360+Fouuus@users.noreply.github.com>
2023-02-20 22:21:44 +0300

    Add files via upload

---
+++ komutlar/diger/sesligirme.js
@@ -0,0 +1,3 @@
+client.on("ready", () => {
+  client.channels.get("814557772385222676").join();
+})
---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTA3MjU4NTY3NTA5MTc0Njg0OA.GdDe2u.Fp4G5rtJETuW851D_s4xR1-cZ3slwpMo38OvIo",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "!", // bot prefix
+    SAHİP: process.env.SAHİP || "1000140980411977809", //sahip id :()
+}
